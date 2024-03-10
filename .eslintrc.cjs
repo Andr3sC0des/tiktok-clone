65fcb738d29f013a2e16981186804bba472d15a8
andr3sc0des <andresqcodes@gmail.com>
2024-03-10 15:56:12 -0600

    Initial Commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: ['standard', 'plugin:astro/recommended'],
+  rules: {
+    'no-tabs': 'off',
+    indent: 'off',
+    'space-before-function-paren': 'off'
+  },
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro']
+      },
+      rules: {}
+    }
+  ]
+}
